a35ee6ea75bf9fd7a5d2413c100a71f62a7bab16
accee00 <harshvardhan78170@gmail.com>
2026-03-06 21:55:41 +0530

    added delete and update along with giving ids to todo.

--- todo-cli/index.js
+++ todo-cli/index.js
@@ -1,4 +1,5 @@
 const { program } = require("commander");
+const { randomUUID } = require("crypto");
 const fs = require("fs");
 
 
@@ -16,6 +17,29 @@ program
         })
     });
 
+program
+    .command("update-todo")
+    .description("Update todo requires id and content.")
+    .argument("<file>", "json file")
+    .argument("<id>", "todo id")
+    .argument("<updated-todo>", "updated todo")
+    .action((file, id, updatedTodo) => {
+        let todos = [];
+        const data = fs.readFileSync(file, "utf-8");
+        if (data) {
+            todos = JSON.parse(data);
+        } else {
+            console.log("No todo found");
+            return;
+        }
+        todos.forEach(todo => {
+            if (todo.id === id) {
+                todo.todo = updatedTodo
+            }
+        });
+        fs.writeFileSync(file, JSON.stringify(todos, null, 2));
+        console.log("Todo updated.");
+    });
 
 program
     .command("add-todo")
@@ -28,10 +52,29 @@ program
         if (data) {
             todos = JSON.parse(data);
         }
-        todos.push({ todo: todo });
+        todos.push({ id: randomUUID(), todo: todo });
         fs.writeFileSync(file, JSON.stringify(todos, null, 2));
         console.log("Todo added.");
     });
 
+program
+    .command("delete-todo")
+    .description("Update todo requires id and content.")
+    .argument("<file>", "json file")
+    .argument("<id>", "todo id")
+    .action((file, id) => {
+        let todos = [];
+        const data = fs.readFileSync(file, "utf-8");
+        if (data) {
+            todos = JSON.parse(data);
+        } else {
+            console.log("No todo found");
+            return;
+        }
+        todos = JSON.parse(data);
+        const updatedTodos = todos.filter(todo => todo.id !== id);
+        fs.writeFileSync(file, JSON.stringify(updatedTodos, null, 2));
+        console.log("Todo removed.");
+    });
 
 program.parse(process.argv);
