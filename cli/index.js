9b40ec4a2d13266dea795963389844ec50964403
accee00 <harshvardhan78170@gmail.com>
2026-03-06 20:39:04 +0530

    created custom CLI to perform operations on file using commander.

---
+++ cli/index.js
@@ -0,0 +1,58 @@
+const { program } = require("commander");
+const fs = require("fs");
+
+program
+    .name("counter")
+    .description("CLI to do file based task.")
+    .version("0.9.9");
+
+program
+    .command("count-words")
+    .description("Count the number of words in a file.")
+    .argument("<file>", "file to count")
+    .action((file) => {
+        fs.readFile(file, "utf-8", (error, data) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            const words = data.split(" ");
+            let count = 0;
+
+            words.forEach(word => {
+                if (word.trim() !== "") {
+                    count++;
+                }
+            });
+            console.log(`There are ${count} words in ${file}`);
+        });
+    });
+
+
+program
+    .command("count-lines")
+    .description("Count the no of lines in a file.")
+    .argument("<file>", "file to count")
+    .action((file) => {
+        fs.readFile(file, "utf-8", (error, data) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            const words = data.split("\n");
+            let count = 0;
+
+            words.forEach(word => {
+                if (word.trim() !== "") {
+                    count++;
+                }
+            });
+            console.log(`There are ${count} lines in ${file}`);
+        });
+    });
+
+program
+    .command("count-vowels")
+    .description("")
+
+program.parse(process.argv);
