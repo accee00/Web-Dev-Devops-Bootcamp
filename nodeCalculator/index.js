fd97a3395fd760db0d3675ee9c71059ac731fd08
accee00 <harshvardhan78170@gmail.com>
2026-03-14 22:01:01 +0530

    add req count

--- nodeCalculator/index.js
+++ nodeCalculator/index.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const app = express();
+let requestCount = 0;
 
+function middleware(req, res, next) {
+    requestCount++;
+    next()
+}
+app.use(middleware);
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
-
 
 app.listen(3000, () => {
     console.log("App is connected to http://localhost:3000");
 });
 
+app.route("/req-count").get((req, res) => {
+    res.json({
+        count: requestCount,
+    });
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.get("/sum", (req, res) => {
     const a = Number(req.query.a);
